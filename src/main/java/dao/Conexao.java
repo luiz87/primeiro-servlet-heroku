3a81397e2ee096723643c450c025e57051b9752f
Luiz Felipe <luizfelipe.freire@gmail.com>
2020-05-29 19:25:59 -0300

    modificações na conexão com o banco de dados

--- src/main/java/dao/Conexao.java
+++ src/main/java/dao/Conexao.java
@@ -8,7 +8,7 @@ public class Conexao {
 	public static Connection getConectar() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conexao = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net/sql10343221", "sql10343221", "eP7GZaD86t");
+			Connection conexao = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net/sql10344227", "sql10344227", "6Z3uZXK7y6");
 			return conexao;
 		} catch (Exception e) {
 			System.out.println("Falha no Banco de Dados");
